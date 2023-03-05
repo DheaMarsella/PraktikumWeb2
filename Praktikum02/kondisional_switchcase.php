6202cdbf330616224177111ec165168bc93e216c
DheaMarsella <114457865+DheaMarsella@users.noreply.github.com>
2023-03-05 20:11:17 +0700

    Add files via upload

---
+++ Praktikum02/kondisional_switchcase.php
@@ -0,0 +1,13 @@
+<?php
+$nasi = "digoreng";
+switch($nasi) {
+    case "dibakar" :
+        echo "nasi bakar";
+        break;
+    case "di goreng" :
+        echo "nasi goreng";
+        break;
+    default: 
+        echo "nasi biasa";
+        break;
+}
