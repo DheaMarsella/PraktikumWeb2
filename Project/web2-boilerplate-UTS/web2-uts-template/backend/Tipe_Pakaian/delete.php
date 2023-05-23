152cccf497c49b6e352578674de70e68f5b579a9
DheaMarsella <114457865+DheaMarsella@users.noreply.github.com>
2023-05-23 08:03:16 +0700

    Delete Project/web2-boilerplate-UTS directory

--- Project/web2-boilerplate-UTS/web2-uts-template/backend/Tipe_Pakaian/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../../database/dbkoneksi.php";
-$id = $_GET['iddel'];
-$sql = "DELETE FROM tipe_pakaian WHERE id = ?";
-$statement = $dbh->prepare($sql); 
-$statement->execute([$id]);
-
-header("location: ./index.php");
